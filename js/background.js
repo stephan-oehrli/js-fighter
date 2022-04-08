fb11a9608d2a5da74af44573f69905e4f5893493
Stephan Oehrli <stephan.oehrli@gmx.ch>
2022-04-08 03:39:58 +0200

    Fix broken image error

--- js/background.js
+++ js/background.js
@@ -2,17 +2,17 @@ class Background {
   constructor() {
     this.background = new Sprite({
       position: { x: 0, y: 0 },
-      imageSrc: '../img/background.png'
+      imageSrc: './img/background.png'
     });
     this.shop = new Sprite({
       position: { x: 600, y: 128 },
-      imageSrc: '../img/shop.png',
+      imageSrc: './img/shop.png',
       scale: 2.75,
       framesMax: 6
     });
     this.latern = new Sprite({
       position: { x: 10, y: 200},
-      imageSrc: '../img/street light.png',
+      imageSrc: './img/street light.png',
       scale: 2.2,
       framesMax: 10,
       framesHold: 25
